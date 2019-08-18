8e84a0d3c9661be01798072d67dde4ef5fee8b2e
Steven Shamlian <github@ttoom.org>
2019-08-18 14:14:00 -0400

    Add move and rotate

--- drive-root.py
+++ drive-root.py
@@ -8,40 +8,44 @@ try:
     robot = Root()
     robot.wait_for_connect()
 
-    print("Press letter (f,b,l,r) to drive robot (t) to turn, (s) to stop, (u or d) raise pen up or down, (z) to get sensor states, (i) for sniff, (q) to quit")
+    print("Press letter (f,b,l,r) to drive robot (t) to turn, (s) to stop, (u or d) raise pen up or down, (m {val}) to move, (a{val}) to rotate, (z) to get sensor states, (i) for sniff, (q) to quit")
     while command != "q" and robot.is_running():
         command = input('> ') # wait for keyboard input
         if command == "f":
-            print ("Drive forward")
+            print("Drive forward")
             robot.set_motor_speeds(100, 100)
         if command == "b":
-            print ("Drive backwards")
+            print("Drive backwards")
             robot.set_motor_speeds(-100, -100)
         if command == "r":
-            print ("Drive right")
+            print("Drive right")
             robot.set_motor_speeds(100, 0)
         if command == "l":
-            print ("Drive left")
+            print("Drive left")
             robot.set_motor_speeds(0, 100)
         if command == "s":
-            print ("Stop")
+            print("Stop")
             robot.set_motor_speeds(0, 0)
         if command == "u":
-            print ("Pen up")
+            print("Pen up")
             robot.set_marker_eraser_pos(robot.marker_up_eraser_up)
         if command == "d":
-            print ("Pen down")
+            print("Pen down")
             robot.set_marker_eraser_pos(robot.marker_down_eraser_up)
-        if command == "t":
-            rate = int(input('Enter turn rate (up to +-90): '))
-            print ("Turning ", rate)
-            if rate >= 0:
-                left = rate
-                right = 0
-            else:
-                left = 0
-                right = -1*rate
-            robot.set_motor_speeds(left, right)
+        if command[0] == "m":
+            try:
+                dist = int(command.split()[1])
+                print("Moving", dist, 'mm')
+                robot.drive_distance(dist)
+            except:
+                print("Bad command")
+        if command[0] == "a":
+            try:
+                angle = int(command.split()[1])
+                print("Rotating", angle, 'decidegrees')
+                robot.rotate_angle(angle)
+            except:
+                print("Bad command")
         if command == "z":
             for s, v in robot.sensor.items():
                 print(s, v)
