a6a79618dbbebf669be34265ab05d5a6afb4a11e
Steven Shamlian <github@ttoom.org>
2019-08-21 22:14:03 -0400

    Added linear approximation of Beziers and Arcs

--- svg_plot.py
+++ svg_plot.py
@@ -15,6 +15,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Plot an SVG path with Root.')
 parser.add_argument('-s', '--scale', type=float, default=1, help='Scalar for points')
+parser.add_argument('-a', '--approximate', type=int, default=5, help='Number of lines to use to approximate curves')
+parser.add_argument('-l', '--min_length', type=float, default=10, help='Minimum curve length for approximation')
+parser.add_argument('-e', '--epsilon', type=float, default=0.5, help='Distance between endpoints to consider "close enough" to not lift the pen')
 parser.add_argument('filename', type=str, help='SVG with path to plot')
 
 args = parser.parse_args()
@@ -33,15 +36,25 @@ try:
     robot.wait_for_connect()
     time.sleep(1) 
 
+    segments = numpy.linspace(0, 1, args.approximate + 1)
+
     for path in paths:
         for element in path:
-            start = invert_y(element.start) * args.scale
-            end   = invert_y(element.end  ) * args.scale
-            if robot.last_coord != start:
-                robot.set_marker_eraser_pos(robot.marker_up_eraser_up)
-                robot.drive_complex(start)
-                robot.set_marker_eraser_pos(robot.marker_down_eraser_up)
-            robot.drive_complex(end)
+            if type(element) == svgpathtools.path.Line or element.length()*args.scale < args.min_length:
+                line_list = [element]
+            else: # adapted from svgpathtools issue #61
+                pts = [element.point(t) for t in segments]
+                line_list = [svgpathtools.Line(pts[i-1], pts[i]) for i in range(1, len(pts))]
+
+            for line in line_list:
+                start = invert_y(line.start) * args.scale
+                end   = invert_y(line.end  ) * args.scale
+                # if the robot's last known position isn't close enough to the start, lift the pen
+                if numpy.linalg.norm(robot.last_coord - start) > args.epsilon:
+                    robot.set_marker_eraser_pos(robot.marker_up_eraser_up)
+                    robot.drive_complex(start)
+                    robot.set_marker_eraser_pos(robot.marker_down_eraser_up)
+                robot.drive_complex(end)
 
     robot.set_marker_eraser_pos(robot.marker_up_eraser_up)
 
