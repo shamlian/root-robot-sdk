296098f24a436c17ce87ee8436a5ed791fa50e09
Steven Shamlian <github@ttoom.org>
2019-11-17 20:55:51 -0500

    Corrected RootPhy class

--- pyroot/phy_base.py
+++ pyroot/phy_base.py
@@ -1,35 +1,19 @@
-# External Dependencies
-import serial
-import queue
-import time
-import threading
-from binascii import hexlify, unhexlify
-
-class RootSerial(object): # TODO: Make RootPhy ABC
+class RootPhy(object): # TODO: Use ABC
 
     def __init__(self, name = None, dev = None, wait_for_connect = True):
-        """Sets up serial port connection with robot.
+        """Sets up physical connection with robot.
         
         Parameters
         ----------
         name : str, optional
-            Name of the robot to connect to; currently not checked.
+            Name of the robot to connect to.
         dev : str, optional
             Name of the device to connect to. If not supplied, will attempt
             to auto-discover.
         wait_for_connect : bool, optional
             If true (default), blocks until a connection is made
         """
-        if dev == None:
-            raise NotImplementedError('Serial port discovery not implemented.')
-
-        self._serial_port = serial.Serial(port=dev, baudrate=115200)
-        self._desired_name = name
-        self._comms_thread = threading.Thread(target = self._maintain_connection)
-        self._comms_thread.start()
-
-        if wait_for_connect:
-            self.wait_for_connect()
+        raise NotImplementedError('__init__ not implemented.')
 
     def wait_for_connect(self, timeout = float('inf')):
         """Blocking function initializing robot connection.
@@ -41,26 +25,15 @@ class RootSerial(object): # TODO: Make RootPhy ABC
             TimeoutError if timeout exceeded.
         """
 
-        timeout += time.time()
-
-        while self._serial_port.is_open() is False and time.time() < timeout:
-            time.sleep(0.1) # wait for a root robot to be attached
-        if self._serial_port.is_open() is False:
-            raise TimeoutError('Timed out waiting for ' + self._desired_name)
-
-        try:
-            self.rx_q = queue.SimpleQueue()
-        except AttributeError:
-            self.rx_q = queue.Queue()
+        raise NotImplementedError('wait_for_connect not implemented.')
 
     def is_connected(self):
-        """Utility function for determining state of serial thread."""
-        return self._comms_thread.is_alive()
+        """Utility function for determining state of comms thread."""
+        raise NotImplementedError('is_connected not implemented.')
 
     def disconnect(self):
         """Disconnects from robot and stops comms thread."""
-        self._serial_port.close()
-        self._comms_thread.join()
+        raise NotImplementedError('disconnect not implemented.')
 
     def send_raw(self, packet):
         """Helper method to send raw packets to the robot.
@@ -70,27 +43,4 @@ class RootSerial(object): # TODO: Make RootPhy ABC
         packet : bytes
             20-byte packet to send to the robot.
         """
-        if len(packet) == 20:
-            try:
-                self._serial_port.write(hexlify(packet) + b'\n')
-            except serial.SerialException as e:
-                print(e)
-        else:
-            print('Error: send_raw: Packet wrong length.')
-
-    def _maintain_connection(self):
-        """Assembles received data into packets for parsing.
-        Safely shuts things down if the connection is interrupted.
-        """
-
-        while self._serial_port.is_open():
-            try:
-                packet = self._serial_port.readline()
-                if len(packet) == 41:
-                    packet = unhexlify(packet[:-1])
-                    self.rx_q.put(packet)
-                else:
-                    print('ERROR: Received packet of improper length')
-            except serial.SerialException as e:
-                print(e)
-                # assuming a SerialExeception causes the serial port to close
+        raise NotImplementedError('send_raw not implemented.')
